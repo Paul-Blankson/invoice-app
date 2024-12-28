46e82c5246782794951b4a5a15eed31b4245afe0
pablo-devs <blanksonpaul3@gmail.com>
2024-12-28 14:30:26 +0000

    feat: add Input properties for variant and heading level

--- src/app/shared/components/headline/headline.component.ts
+++ src/app/shared/components/headline/headline.component.ts
@@ -4,11 +4,12 @@ import { HeadingLevel, HeadingVariant } from '../../models';
 
 @Component({
   selector: 'app-headline',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './headline.component.html',
   styleUrl: './headline.component.css'
 })
 export class HeadlineComponent {
   @Input() level: HeadingLevel = 'h3';
-  @Input() variant: HeadingVariant = 'medium';
+  @Input() variant: HeadingVariant = 'large';
 }
