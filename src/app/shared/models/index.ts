8b5911b8bd646432c4c7b80b62c51f17b57cb792
pablo-devs <blanksonpaul3@gmail.com>
2024-12-28 17:28:40 +0000

    feat: add IconVariant and IconName types

--- src/app/shared/models/index.ts
+++ src/app/shared/models/index.ts
@@ -1,2 +1,16 @@
 export type HeadingLevel = 'h1' | 'h2' | 'h3';
 export type TextVariant = 'large' | 'small';
+export type IconVariant = 'mobile' | 'tablet' | 'desktop';
+export type IconName =
+  | 'icon-sun'
+  | 'icon-moon'
+  | 'icon-arrow-down'
+  | 'icon-arrow-left'
+  | 'icon-arrow-right'
+  | 'icon-calender'
+  | 'icon-check'
+  | 'icon-delete'
+  | 'icon-plus'
+  | 'illustration-empty'
+  | 'image-avatar'
+  | 'logo';
