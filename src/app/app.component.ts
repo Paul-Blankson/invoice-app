e709dd08d1ca151d8110d2f24dc760db4618dbdf
pablo-devs <blanksonpaul3@gmail.com>
2024-12-29 23:09:19 +0000

    feat: import IconComponent to be used in template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IconComponent } from "./shared/components/icon/icon.component";
 
 @Component({
   selector: 'app-root',
-  imports: [ RouterOutlet ],
+  imports: [RouterOutlet, IconComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
