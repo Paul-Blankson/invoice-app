30ff2adc8f303d7fa741b8681eaec264e4e4b476
pablo-devs <blanksonpaul3@gmail.com>
2024-12-30 17:05:38 +0000

    feat(button): add button component with variants and styles
    - Implemented ButtonComponent with @Input for button variants
    - Added IconComponent and HeadlineComponent as imports
    - Defined button text based on variant using getButtonText method
    - Applied styles for different button variants including hover effects
    - Included conditional rendering for icon in 'btn1' variant

---
+++ src/app/shared/components/button/button.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input } from '@angular/core';
+import { ButtonVariant } from '../../models';
+import { IconComponent } from "../icon/icon.component";
+import { HeadlineComponent } from "../headline/headline.component";
+
+@Component({
+  selector: 'app-button',
+  imports: [IconComponent, HeadlineComponent],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css'
+})
+export class ButtonComponent {
+  @Input() btnVariant: ButtonVariant = 'btn1';
+
+  get buttonClass(): string {
+    return this.btnVariant;
+  }
+
+  getButtonText(): string {
+    const textMap = {
+      'btn1': 'New Invoice',
+      'btn2': 'Mark as Paid',
+      'btn3': 'Edit',
+      'btn4': 'Save as Draft',
+      'btn5': 'Delete',
+      'btn6': '+ Add New Item'
+    };
+    return textMap[this.btnVariant];
+  }
+}
