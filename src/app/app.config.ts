3444b4d285c9cbdd6b29255d05baba690727d8c1
pablo-devs <blanksonpaul3@gmail.com>
2025-01-01 17:00:37 +0000

    refactor(invoice-effect): import provideHttpClient in the app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,6 +7,7 @@ import { provideRouter } from '@angular/router';
 import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 import { themeFeature } from './store/reducers/theme.reducer';
 import { invoiceFeature } from './store/reducers/invoice.reducer';
@@ -16,6 +17,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideHttpClient(),
     provideStore({
       [themeFeature.name]: themeFeature.reducer,
       [invoiceFeature.name]: invoiceFeature.reducer
