76c605f48e4c44117ddba6a41cee4bbbbf15f1de
pablo-devs <blanksonpaul3@gmail.com>
2025-01-02 15:32:14 +0000

    feat: Add InvoiceListComponent with invoice cards display
    - Add OnInit lifecycle hook to InvoiceListComponent
    - Fetch and display invoices using NgRx store
    - Add headline, text, filter, and button components for UI
    - Implement responsive design for different screen sizes
    - Add CSS styles for invoice list and header

--- src/app/pages/invoice-list/invoice-list.component.ts
+++ src/app/pages/invoice-list/invoice-list.component.ts
@@ -1,16 +1,36 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { HeadlineComponent } from "../../shared/components/headline/headline.component";
 import { TextComponent } from "../../shared/components/text/text.component";
 import { FilterComponent } from "../../shared/components/filter/filter.component";
 import { ButtonComponent } from "../../shared/components/button/button.component";
 import { InvoiceCardComponent } from "../../shared/components/invoice-card/invoice-card.component";
+import { map, Observable } from 'rxjs';
+import { Invoice } from '../../models/services.type';
+import { Store } from '@ngrx/store';
+import { selectInvoices, selectLoading } from '../../store/reducers/invoice.reducer';
+import { InvoiceActions } from '../../store/actions/invoice.actions';
 
 @Component({
   selector: 'app-invoice-list',
-  imports: [HeadlineComponent, TextComponent, FilterComponent, ButtonComponent, InvoiceCardComponent],
+  imports: [CommonModule, HeadlineComponent, TextComponent, FilterComponent, ButtonComponent, InvoiceCardComponent],
   templateUrl: './invoice-list.component.html',
   styleUrl: './invoice-list.component.css'
 })
-export class InvoiceListComponent {
+export class InvoiceListComponent implements OnInit {
+  invoices$: Observable<Invoice[]>;
+  loading$: Observable<boolean>;
+  invoiceCount$: Observable<number>;
 
+  constructor(private readonly store: Store) {
+    this.invoices$ = this.store.select(selectInvoices);
+    this.loading$ = this.store.select(selectLoading);
+    this.invoiceCount$ = this.invoices$.pipe(
+      map(invoices => invoices.length)
+    );
+  }
+
+  ngOnInit(): void {
+    this.store.dispatch(InvoiceActions.loadInvoices());
+  }
 }
