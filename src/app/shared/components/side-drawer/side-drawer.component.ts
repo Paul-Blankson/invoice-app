70b773f3b54fd5ff918892d381b4f8feac84c8d9
pablo-devs <blanksonpaul3@gmail.com>
2025-01-09 18:14:05 +0000

    feat: implement SideDrawerComponent with click outside functionality
    - Create SideDrawerComponent with inputs for isDrawerOpen and title
    - Add output event emitter for close
    - Implement onClickOutside method to close drawer when clicking outside
    - Add ClickOutsideDirective to handle outside clicks
    - Add HTML and CSS for side drawer layout and styles

--- src/app/shared/components/side-drawer/side-drawer.component.ts
+++ src/app/shared/components/side-drawer/side-drawer.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ClickOutsideDirective } from '../../directives/click-outside.directive';
+import { HeadlineComponent } from "../headline/headline.component";
+import { IconComponent } from "../icon/icon.component";
 
 @Component({
   selector: 'app-side-drawer',
-  imports: [],
+  imports: [CommonModule, ClickOutsideDirective, HeadlineComponent, IconComponent],
   templateUrl: './side-drawer.component.html',
   styleUrl: './side-drawer.component.css'
 })
 export class SideDrawerComponent {
+  @Input() isDrawerOpen: boolean = false;
+  @Input() title: string = '';
+  @Output() close = new EventEmitter<void>();
 
+  onClickOutside() {
+    if (this.isDrawerOpen) {
+      this.onClose();
+    }
+  }
+
+  onClose() {
+    this.close.emit();
+  }
 }
