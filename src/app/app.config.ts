260485f8f0bba8c49dea7dba19a9de88a31adf07
pablo-devs <blanksonpaul3@gmail.com>
2025-01-09 09:07:03 +0000

    refactor: import provideAnimations and added it to providers array

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,6 +8,7 @@ import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 import { themeFeature } from './store/reducers/theme.reducer';
 import { invoiceFeature } from './store/reducers/invoice.reducer';
@@ -19,6 +20,7 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
+    provideAnimations(),
     provideStore({
       [themeFeature.name]: themeFeature.reducer,
       [invoiceFeature.name]: invoiceFeature.reducer,
