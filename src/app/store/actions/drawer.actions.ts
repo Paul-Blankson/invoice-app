2439153d9a8d6b7057e21d248a1776dc2e9e8982
pablo-devs <blanksonpaul3@gmail.com>
2025-02-02 18:46:02 +0000

    fix: removed unused drawer state

--- src/app/store/actions/drawer.actions.ts
+++
@@ -1,4 +0,0 @@
-import { createAction } from '@ngrx/store';
-
-export const openDrawer = createAction('[Drawer] Open');
-export const closeDrawer = createAction('[Drawer] Close');
--- src/app/store/selectors/theme.selectors.ts
+++
@@ -1,4 +0,0 @@
-import { themeFeature } from '../reducers/theme.reducer';
-
-export const selectThemeState = themeFeature.selectThemeState;
-export const selectCurrentTheme = themeFeature.selectTheme;
